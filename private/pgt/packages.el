669e30a1c7361d8bee113c81f0e25140e3204376
Prem Thomas <prem.thomas@yale.edu>
2016-03-07 00:59:39 -0500

    Keybindings for windows; time display changes.

--- private/pgt/packages.el
+++ private/pgt/packages.el
@@ -16,7 +16,6 @@
 ;;
 ;;   SPC h SPC layers RET
 ;;
-;;
 ;; Briefly, each package to be installed or configured by this layer should be
 ;; added to `pgt-packages'. Then, for each package PACKAGE:
 ;;
@@ -57,17 +56,34 @@ Each entry is either:
 
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+(setq calendar-latitude 41.3456)
+(setq calendar-longitude -72.9722)
+(setq calendar-location-name "Hamden, CT")
+
+
 (global-set-key (kbd "<s-up>") 'beginning-of-buffer)
 (global-set-key (kbd "<s-down>") 'end-of-buffer)
 (global-set-key (kbd "<s-right>") 'move-end-of-line)
 (global-set-key (kbd "<s-left>") 'move-beginning-of-line)
 
 (global-set-key (kbd "s-0") 'delete-window)
+(global-set-key (kbd "s-1") 'delete-other-windows)
 (global-set-key (kbd "s-2") 'split-window-vertically)
 (global-set-key (kbd "s-3") 'split-window-horizontally)
+(global-set-key (kbd "s-i") 'other-window)
 
-;(global-set-key (kbd "s-B") 'ibuffer)
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
+(global-set-key (kbd "s-b") 'ido-switch-buffer)
 (global-set-key (kbd "s-B") 'ibuffer)
 (spacemacs/set-leader-keys
   "bl"  'ibuffer)
+
+(setq display-time-day-and-date t)
+(setq display-time-default-load-average nil)
+(format-time-string "%a %e %B --")
+(setq display-time-format "%I:%M %p %e %b %y %_5j   ")
+(display-time)
+(delq 'display-time-string global-mode-string)
+(setq frame-title-format '((buffer-file-name "%f" ("%b")) "    " display-time-string) )
 ;;; packages.el ends here
